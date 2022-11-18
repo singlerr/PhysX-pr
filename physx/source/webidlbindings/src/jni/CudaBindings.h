f0c4e32040c8a7c26ac6d9127742c7f382974265
fabmax <fabmax.thiele@gmail.com>
2022-11-18 20:01:12 +0100

    gpu rigid bodies

---
+++ physx/source/webidlbindings/src/jni/CudaBindings.h
@@ -0,0 +1,8 @@
+#ifndef CUDA_BINDINGS_H
+#define CUDA_BINDINGS_H
+
+#include "PxPhysicsAPI.h"
+
+typedef physx::PxCudaInteropMode::Enum PxCudaInteropModeEnum;
+
+#endif
--- physx/source/webidlbindings/src/jni/PhysXJni.cpp
+++ physx/source/webidlbindings/src/jni/PhysXJni.cpp
@@ -1,4 +1,5 @@
 #include "../common/WebIdlBindings.h"
+#include "CudaBindings.h"
 #include "PhysXJniGlue.h"
 
 // nothing else to do here, the magic happens inside PhysXJniGlue.h
